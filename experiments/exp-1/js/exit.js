44e8c09645b6f4cb28cdb30770dc08805674b33b
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2024-08-31 18:30:41 -0400

    add exit debrief

--- experiments/exp-1/js/exit.js
+++ experiments/exp-1/js/exit.js
@@ -1,9 +1,10 @@
-// exit survey
-async function createExitSurvey() {
+// exit survey and debrief
+async function createExit() {
   try {
     const response = await fetch("html/exit-survey.html");
     const text = await response.text();
-    return {
+    const exit = [];
+    const exit_survey = {
       type: jsPsychSurveyHtmlForm,
       preamble: `
         <p>You have reached the end of the experiment! To collect your bonus, please complete the following questions. Your answer to these questions will not affect your bonus, so please answer honestly.</p>
@@ -11,6 +12,19 @@ async function createExitSurvey() {
       html: text,
       button_label: ["Continue, save data, and collect bonus!"],
     };
+    exit.push(exit_survey);
+
+    const debrief = {
+      type: jsPsychHtmlKeyboardResponse,
+      stimulus: `<p>Thanks for participating in the experiment!</p>
+                    <p><a href="https://app.prolific.co/submissions/complete?cc=C1E1PWV8">Click here to return to Prolific and complete the study</a>.</p>
+                    <p>It is now safe to close the window. Your pay will be delivered within a few days.</p>
+                    `,
+      choices: "NO_KEYS",
+    };
+    exit.push(debrief);
+
+    return exit;
   } catch (error) {
     console.error("Error creating exit survey:", error);
     throw error;
