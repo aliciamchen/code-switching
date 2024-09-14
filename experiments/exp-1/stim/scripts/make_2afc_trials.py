2a85598ae640b03f0379343b8d05042ffc1e7292
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2024-09-14 18:09:53 -0400

    new way of generating 2afc trials

--- experiments/exp-1/stim/scripts/make_2afc_trials.py
+++ experiments/exp-1/stim/scripts/make_2afc_trials.py
@@ -11,121 +11,121 @@ def separate_tangrams(lexicon):
     return shared_tangrams, unique_tangrams
 
 
-def generate_all_unique_sets(lexicon, shared_tangrams, unique_tangrams):
-    """Generate all unique sets of trials"""
-
-    # what are the tangrams in 'unique'?
+def generate_main_trials(lexicon, shared_tangrams, unique_tangrams):
     unique_set = list(set([entry["tangram"] for entry in unique_tangrams]))
     shared_set = list(set([entry["tangram"] for entry in shared_tangrams]))
 
-    all_sets = []
-    shared_permutations = list(itertools.permutations(shared_set))
+    # generate list of all possible pairs of tangram, where there is one shared and one unique tangram
+    tangram_pairs = list(itertools.product(shared_set, unique_set))
 
-    # Generate all combinations of the valid pairs of audiences
-    audience_pairs = [("one", "both"), ("both", "one")]
-    audience_combs = list(itertools.product(audience_pairs, repeat=3))
-    # get rid of the combinations that have three of the same element
-    audience_combs = [
-        audience_comb
-        for audience_comb in audience_combs
-        if len(set(audience_comb)) == 2
-    ]
-
-    for shared_perm in shared_permutations:
-        for comb in audience_combs:
-            # print(comb)
-            trials = []
-            for i in range(3):
-                trial_red_refer = {
-                    "goal": "refer",
-                    "audience": comb[i][0],
-                    "audience_group": "red",
-                    "shared": [
-                        entry
-                        for entry in lexicon
-                        if (entry["group"] == "red")
-                        and (entry["tangram"] == shared_perm[i])
-                    ][0],
-                    "unique": [
-                        entry
-                        for entry in lexicon
-                        if (entry["group"] == "red")
-                        and (entry["tangram"] == unique_set[i])
-                    ][0],
+    trials = []
+    for tangram_pair in tangram_pairs:
+        for audience_group in ["red", "blue"]:
+            for goal in ["refer", "social"]:
+                trial = {
+                    "type": "main",
+                    "goal": goal,
+                    "audience": "one",
+                    "audience_group": audience_group,
+                    "options": [
+                        [
+                            entry
+                            for entry in lexicon
+                            if (entry["group"] == audience_group)
+                            and (entry["tangram"] == tangram_pair[0])
+                        ][0],
+                        [
+                            entry
+                            for entry in lexicon
+                            if (entry["group"] == audience_group)
+                            and (entry["tangram"] == tangram_pair[1])
+                        ][0],
+                    ],
                 }
-                trial_red_social = {
-                    "goal": "social",
-                    "audience": comb[i][0],
-                    "audience_group": "red",
-                    "shared": [
-                        entry
-                        for entry in lexicon
-                        if (entry["group"] == "red")
-                        and (entry["tangram"] == shared_perm[i])
-                    ][0],
-                    "unique": [
-                        entry
-                        for entry in lexicon
-                        if (entry["group"] == "red")
-                        and (entry["tangram"] == unique_set[i])
-                    ][0],
-                }
-                trial_blue_refer = {
-                    "goal": "refer",
-                    "audience": comb[i][1],
-                    "audience_group": "blue",
-                    "shared": [
-                        entry
-                        for entry in lexicon
-                        if (entry["group"] == "blue")
-                        and (entry["tangram"] == shared_perm[i])
-                    ][0],
-                    "unique": [
-                        entry
-                        for entry in lexicon
-                        if (entry["group"] == "blue")
-                        and (entry["tangram"] == unique_set[i])
-                    ][0],
-                }
-                trial_blue_social = {
-                    "goal": "social",
-                    "audience": comb[i][1],
-                    "audience_group": "blue",
-                    "shared": [
-                        entry
-                        for entry in lexicon
-                        if (entry["group"] == "blue")
-                        and (entry["tangram"] == shared_perm[i])
-                    ][0],
-                    "unique": [
-                        entry
-                        for entry in lexicon
-                        if (entry["group"] == "blue")
-                        and (entry["tangram"] == unique_set[i])
-                    ][0],
-                }
-                trials.append(trial_red_refer)
-                trials.append(trial_red_social)
-                trials.append(trial_blue_refer)
-                trials.append(trial_blue_social)
-            all_sets.append(trials)
-    return all_sets
+                trials.append(trial)
+    assert len(trials) == 36
+    return trials
 
 
-def main(n_items):
-    for item_num in range(n_items):
+def generate_control_trials(
+    this_lexicon, other_lexicon, shared_tangrams, unique_tangrams
+):
+    """other_lexicon is lexicon of another (counterbalanced) items to get the labels that are not seen"""
+    unique_set = list(set([entry["tangram"] for entry in unique_tangrams]))
+    shared_set = list(set([entry["tangram"] for entry in shared_tangrams]))
+
+    shared_control_trials = []
+    for shared_tangram in shared_set:
+        for audience_group in ["red", "blue"]:
+            this_option = [
+                entry
+                for entry in this_lexicon
+                if (entry["group"] == audience_group)
+                and (entry["tangram"] == shared_tangram)
+            ][0]
+            unseen_label_option = [
+                entry
+                for entry in other_lexicon
+                if (entry["tangram"] == shared_tangram)
+                and (entry["label"] != this_option["label"])
+            ][0]
+
+            trial = {
+                "type": "control",
+                "goal": "refer",
+                "audience": "one",
+                "audience_group": audience_group,
+                "options": [this_option, unseen_label_option],
+            }
+            shared_control_trials.append(trial)
+
+    unique_control_trials = []
+    for unique_tangram in unique_set:
+        for audience_group in ["red", "blue"]:
+            red_label_option = [
+                entry
+                for entry in this_lexicon
+                if (entry["group"] == "red") and (entry["tangram"] == unique_tangram)
+            ][0]
+            blue_label_option = [
+                entry
+                for entry in this_lexicon
+                if (entry["group"] == "blue") and (entry["tangram"] == unique_tangram)
+            ][0]
+
+            trial = {
+                "type": "control",
+                "goal": "social",
+                "audience": "one",
+                "audience_group": audience_group,
+                "options": [red_label_option, blue_label_option],
+            }
+            unique_control_trials.append(trial)
+
+    return shared_control_trials + unique_control_trials
+
+
+def main(items):
+    for item_num in items:
         with open(f"../items/item_{item_num}_lexicon.json", "r") as f:
             lexicon = json.load(f)
+        other_item_num = 1 if item_num == 0 else 0
+        with open(f"../items/item_{other_item_num}_lexicon.json", "r") as f:
+            other_lexicon = json.load(f)
 
         shared_tangrams, unique_tangrams = separate_tangrams(lexicon)
-        all_sets = generate_all_unique_sets(lexicon, shared_tangrams, unique_tangrams)
-        print(f"Generated {len(all_sets)} sets of trials for item {item_num}")
+        main_trials = generate_main_trials(lexicon, shared_tangrams, unique_tangrams)
+        control_trials = generate_control_trials(
+            lexicon, other_lexicon, shared_tangrams, unique_tangrams
+        )
+        trials = main_trials + control_trials
+        print(f"Generated {len(trials)} sets of trials for item {item_num}")
         with open(f"../2AFC_trials/item_{item_num}_2AFC.json", "w") as json_file:
             # note that each index of this list is a set of trials for a single participant
-            json.dump(all_sets, json_file, indent=2)
+            json.dump(trials, json_file, indent=2)
             print(f"Saved in ../2AFC_trials/item_{item_num}_2AFC.json")
 
 
 if __name__ == "__main__":
-    main(10)
+    main([0, 1])
     print("Done")
