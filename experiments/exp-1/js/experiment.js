024c641b04789d08be44bdeffc2c51cc03ce2989
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2024-09-16 17:37:09 -0400

    get condition assignment from datapipe

--- experiments/exp-1/js/experiment.js
+++ experiments/exp-1/js/experiment.js
@@ -3,13 +3,8 @@
 const local_testing = false;
 
 const params = {
-  perTrialBonus: 0.25,
   n_tangrams: 6
 }
-
-// TODO: get item id and participant id from datapipe condition assignment
-var item_id = 0; // which item (set of stimuli?)
-// TODO later: use datapipe to assign item id
 
 const jsPsych = initJsPsych({
   on_finish: function () {
@@ -21,26 +16,16 @@ const jsPsych = initJsPsych({
   show_progress_bar: true,
 });
 
-
 var subject_id =
   local_testing || jsPsych.data.getURLVariable("PROLIFIC_PID") == undefined
     ? jsPsych.randomization.randomID(12)
     : jsPsych.data.getURLVariable("PROLIFIC_PID");
-console.log("subject_id:", subject_id);
 var study_id = jsPsych.data.getURLVariable("STUDY_ID");
 var session_id = jsPsych.data.getURLVariable("SESSION_ID");
 
-jsPsych.data.addProperties({
-  subject_id: subject_id,
-  study_id: study_id,
-  session_id: session_id,
-  item_id: item_id,
-  url: window.location.href,
-});
-
 var timeline = [];
 
-makeTrials(item_id, jsPsych).then((trials) => {
+makeTrials(jsPsych).then((trials) => {
   timeline.push(trials);
   jsPsych.run(timeline.flat());
 });
--- experiments/exp-1/js/trials.js
+++ experiments/exp-1/js/trials.js
@@ -1,6 +1,19 @@
 // put all the trials together
-async function makeTrials(item_id, jsPsych) {
+async function makeTrials(jsPsych) {
   try {
+    const condition = await jsPsychPipe.getCondition("ZycTOU10DI0v");
+    const item_id = condition % 2;
+    console.log(condition, item_id);
+
+    jsPsych.data.addProperties({
+      subject_id: subject_id,
+      study_id: study_id,
+      session_id: session_id,
+      condition: condition,
+      item_id: item_id,
+      url: window.location.href,
+    });
+
     const timeline = [];
 
     // preload all of the videos in stim/convo_vids/videos/480p15 if they correspond to the item_id
@@ -23,10 +36,7 @@ async function makeTrials(item_id, jsPsych) {
     timeline.push(videoTrials);
 
     // selection phase
-    const selectionTrials = await createSelectionTrials(
-      item_id,
-      jsPsych
-    );
+    const selectionTrials = await createSelectionTrials(item_id, jsPsych);
     timeline.push(selectionTrials);
 
     // exit survey
