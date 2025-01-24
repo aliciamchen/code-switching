4c278b838a6e85f2a3a48fc77ef9eae5afbff9f3
Alicia Chen <amqchen@gmail.com>
2025-01-24 15:34:49 -0500

    clean up notebooks and change output file location

--- model/utils.py
+++ model/utils.py
@@ -1,6 +1,9 @@
 import jax
 import jax.numpy as jnp
 
+from enum import IntEnum
+import seaborn as sns
+
 @jax.jit
 def compute_NLL(preds, responses):
     eps = 1e-7
@@ -9,3 +12,12 @@ def compute_NLL(preds, responses):
         responses * jnp.log(preds_clipped)
         + (1 - responses) * jnp.log(1 - preds_clipped)
     )
+
+fig_dir = "../figures/outputs/"
+
+colors = sns.color_palette("colorblind")
+palette = {"refer to in-group": colors[0], "refer to mixed": colors[2], "social": colors[1]}
+
+class ModelType(IntEnum):
+    base = 0
+    social = 1
